f8a50d472db750c97c9c2e969daa54edac9ab97c
Terje Bråten <terje.braten@sportlobster.com>
2016-07-11 00:07:37 +0100

    Wiremock for github is in place

--- features/bootstrap/WiremockContext.php
+++ features/bootstrap/WiremockContext.php
@@ -10,10 +10,16 @@ use WireMock\Client\WireMock;
 class WiremockContext implements Context
 {
     protected $wiremock;
+    protected $baseUrl;
 
     public function __construct($host='localhost', $port=9090)
     {
         $this->wiremock = WireMock::create($host, $port);
+
+        $this->baseUrl = 'http://' . $host;
+        if ($port) {
+            $this->baseUrl .= ':' . $port;
+        }
 
         // Wait for wiremock to be running and ready
         if (!$this->wiremock->isAlive()) {
@@ -48,10 +54,48 @@ class WiremockContext implements Context
         $this->wiremock->reset();
     }
 
+    private function setGETStubFor($uri, $data)
+    {
+        $this->wiremock->stubFor(
+            WireMock::get(WireMock::urlEqualTo($uri))
+                ->willReturn(
+                    WireMock::aResponse()
+                        ->withHeader('Content-Type', 'application/json; charset=utf-8')
+                        ->withBody(json_encode($data, JSON_UNESCAPED_SLASHES))
+                )
+        );
+    }
+
     /**
      * @Given the github project :project has these contributors:
      */
     public function theGithubProjectHasTheseContributors($project, TableNode $table)
     {
+        $contributorsUri = '/repos/' . $project . '/contributors';
+
+        $contributors = [];
+
+        $index = 4321; // just a random number
+
+        foreach ($table->getHash() as $row) {
+            $username = $row['username'];
+            $location = $row['location'];
+
+            $userUri = '/users/' . $username;
+            $contributor = (object)[
+                'login' => $username,
+                'id' => ++$index,
+                'url' => $this->baseUrl . $userUri,
+                'html_url' => 'a value we do not use',
+                'type' => 'User'
+            ];
+            $contributors[] = $contributor;
+
+            $userData = clone $contributor;
+            $userData->location = $location;
+            $this->setGETStubFor($userUri, $userData);
+        }
+
+        $this->setGETStubFor($contributorsUri, $contributors);
     }
 }
